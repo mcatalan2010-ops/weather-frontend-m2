def8bbc5a39fdf85aaf42592b9727a292b0238a9
mcatalan2010-ops <mcatalan2010@gmail.com>
2025-11-27 21:49:28 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// manejo del DOM con js 
+//queryselectorall selecciona todos los elementos. En este caso todos los que tienen clase nav-link
+var links = document.querySelectorAll('.nav-link');
+console.log(links);
+
+links.forEach(function(link){
+    if(link.href === window.location.href){
+        link.classList.add('active');
+    } else {
+        link.classList.remove('active');
+    }
+    });
+
+
+    // manejo de evento
+    var cardLinks = document.querySelectorAll('.card-link');
+//console.log(cardLinks);
+cardLinks.forEach(function (link){
+    link.addEventListener('click', function() {
+        window.location.href = './detalle.html';
+    });
+});
